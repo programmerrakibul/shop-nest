e9dc8945b21108a6e9291258831bb2c2f20dfa9e
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-01-15 15:12:06 +0600

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to ShopNest Backend!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
